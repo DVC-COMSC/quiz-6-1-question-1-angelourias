d4c3a129b4201dac5d681a5385928e270724cf90
angelourias <angelou02005@gmail.com>
2024-03-14 18:43:38 -0700

    c

--- main.hpp
+++ main.hpp
@@ -2,6 +2,7 @@
 
 void getTwoValues(int& begin, int& end) {
     do {
-        
-    }
+     std::cout << "Enter two integer values (begin and end):";
+     std::cin >> begin >> end;   
+    } while (begin >= end);
 }
