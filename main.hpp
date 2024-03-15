4aa06abb927010f077d4a6dc1b06e9be644ac3e6
angelourias <angelou02005@gmail.com>
2024-03-14 18:46:15 -0700

    c

--- main.hpp
+++ main.hpp
@@ -6,3 +6,13 @@ void getTwoValues(int& begin, int& end) {
      std::cin >> begin >> end;   
     } while (begin >= end);
 }
+
+bool isPrime(int num) {
+    if (num <= 1)
+    return false;
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0)
+        return false;
+    }
+    return true;
+}
